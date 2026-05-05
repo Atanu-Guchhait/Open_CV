7f885453a3ae3d2f819b8aaa1f2fa2a6c08bb150
Atanu Guchhait <atanuguchhait721437@gmail.com>
2026-05-05 16:33:26 +0530

    Add Phase_6 code files

---
+++ Phase_6/2_Thresholding.py
@@ -0,0 +1,11 @@
+import cv2
+
+image = cv2.imread(r"Images\flower.jpg", cv2.IMREAD_GRAYSCALE)
+
+ret, thresholded_image = cv2.threshold(image, 120, 255, cv2.THRESH_BINARY)
+
+cv2.imshow("Original Image", image)
+cv2.imshow("Edges", thresholded_image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
